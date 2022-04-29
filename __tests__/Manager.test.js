2ed1628a7d79b839c93147ef75c0a4e813b752a4
Kiersten Zug <kierstenzug@gmail.com>
2022-04-28 23:27:55 -0700

    Jest tests for Manager.js

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,22 @@
 const Manager = require("../lib/Manager");
+
+const managerName = "Michael Scott";
+const managerId = "1";
+const managerOfficeNum = "4";
+const managerEmail = "bestboss@dundermifflin.com";
+
+describe("Manager", () => {
+  test("should return true", () => {
+    const testManager = new Manager(
+      managerId,
+      managerName,
+      managerEmail,
+      managerOfficeNum
+    );
+
+    expect(testManager.name).toEqual(managerName);
+    expect(testManager.id).toEqual(managerId);
+    expect(testManager.email).toEqual(managerEmail);
+    expect(testManager.officeNumber).toEqual(managerOfficeNum);
+  });
+});
