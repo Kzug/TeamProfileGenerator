2089990d760f615c446d5a9596ffb5aa03204263
Kiersten Zug <kierstenzug@gmail.com>
2022-04-28 23:36:12 -0700

    Added tests for Engineer Class

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,11 +1,22 @@
 const Engineer = require("../lib/Engineer");
 
-const testGithub = "JohnDoe";
+const engineerName = "Dwight Shrute";
+const engineerId = "001";
+const engineerEmail = "beatsbears@galactica.com";
+const testGithub = "github/DShrute";
 
 describe("Engineer", () => {
   test("should return true", () => {
-    const testEngineer = new Engineer(testGithub);
+    const testEngineer = new Engineer(
+      engineerId,
+      engineerName,
+      engineerEmail,
+      testGithub
+    );
 
+    expect(testEngineer.name).toEqual(engineerName);
+    expect(testEngineer.id).toEqual(engineerId);
+    expect(testEngineer.email).toEqual(engineerEmail);
     expect(testEngineer.github).toEqual(testGithub);
   });
 });
