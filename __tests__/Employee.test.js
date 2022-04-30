4b09876252171888b912311a9ec309b8e85e4a3e
Kiersten Zug <kierstenzug@gmail.com>
2022-04-30 09:04:21 -0700

    Add final version of jest tests for the employee class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,12 +4,28 @@ const testId = 1;
 const testName = "frodo";
 const testEmail = "frodo@shire.com";
 
-describe("Employee", () => {
+describe("Employee name", () => {
   test("should return true", () => {
     const testEmployee = new Employee(testId, testName, testEmail);
 
     expect(testEmployee.getName()).toEqual(testName);
     expect(testEmployee.id).toEqual(testId);
     expect(testEmployee.email).toEqual(testEmail);
+  });
+});
+
+describe("Employee ID", () => {
+  test("should return true", () => {
+    const testEmployee = new Employee(testId, testName, testEmail);
+
+    expect(testEmployee.getId()).toEqual(testId);
+  });
+});
+
+describe("Employee e-mail", () => {
+  test("should return true", () => {
+    const testEmployee = new Employee(testId, testName, testEmail);
+
+    expect(testEmployee.getEmail()).toEqual(testEmail);
   });
 });
